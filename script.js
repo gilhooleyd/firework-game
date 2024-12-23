5449a04e6cadea325aa618b291c3766d888fdf44
David Gilhooley <djgilhooley@gmail.com>
2024-12-23 15:29:01 -0500

    Add deck

--- script.js
+++ script.js
@@ -49,9 +49,41 @@ function numberOnClick(number) {
   }
 }
 
+var screen = document.createElement("div");
+screen.style.display = "flex";
+
+var hints = document.createElement("div");
+
+var play = document.createElement("div");
+play.innerHTML = `<h1 style="text-align: center"> Play </h1>`;
+setStyle(play, {
+  display: "inline-block",
+  border: "solid 3px black",
+  width: 150,
+  height: 75,
+  margin: 10,
+  "border-radius": "5px",
+  "box-shadow": "5px 5px 3px lightgray",
+});
+play.onclick = function() {
+  for (var p of players) {
+    for (let c of p.cards) {
+      if (!c.selected) continue;
+      c.number = null;
+      c.color = null;
+      c.selected = false;
+      displayCard(c);
+    }
+  }
+};
+setRaise(play);
+hints.appendChild(play);
+
+
 var discard = document.createElement("div");
 discard.innerHTML = `<h1 style="text-align: center"> Discard </h1>`;
 setStyle(discard, {
+  display: "inline-block",
   border: "solid 3px black",
   width: 150,
   height: 75,
@@ -71,7 +103,7 @@ discard.onclick = function() {
   }
 };
 setRaise(discard);
-document.body.appendChild(discard);
+hints.appendChild(discard);
 
 var COLORS = ["blue", "white", "green", "yellow", "red"];
 var COLOR_HEX = {
@@ -102,7 +134,7 @@ for (let color of COLORS) {
   setRaise(div);
   buttons.appendChild(div);
 }
-document.body.appendChild(buttons);
+hints.appendChild(buttons);
 console.log("added buttons")
 
 // Numbers.
@@ -118,7 +150,7 @@ for (let i = 1; i <= 5; i++) {
   setRaise(div);
   numbers.appendChild(div);
 }
-document.body.appendChild(numbers);
+hints.appendChild(numbers);
 
 
 // Cards.
@@ -140,6 +172,9 @@ function displayCard(card) {
 
 var players = []
 for (var i = 0; i < 4; i++) {
+  var handDiv = document.createElement("div");
+  handDiv.style.display = "flex";
+
   var container = document.createElement("div");
   container.style.display = "flex";
   container.style.margin = "10px";
@@ -168,11 +203,72 @@ for (var i = 0; i < 4; i++) {
     container.appendChild(div);
     player.cards.push(card)
   }
+  handDiv.appendChild(container);
+
+  player.handDiv = handDiv
   players.push(player);
-  var player = document.createElement("input");
-  setStyle(player, {"font-size": "22px", "margin-left": "10px", border: "none", });
-  player.value = `Player ${i}`;
-  document.body.appendChild(player);
-  document.body.appendChild(container);
+  var playerName = document.createElement("input");
+  setStyle(playerName, {"font-size": "22px", "margin-left": "10px", border: "none", });
+  playerName.value = `Player ${i}`;
+  hints.appendChild(playerName);
+  hints.appendChild(handDiv);
+}
+screen.appendChild(hints);
+
+
+// Start of actual game.
+var CARD_AMOUNT = [3, 2, 2, 2, 1];
+var deck = [];
+for (var c of COLORS) {
+  for (var i = 0; i < 5; i++) {
+    for (var a = 0; a < CARD_AMOUNT[i]; a++) {
+      deck.push({ color: c, number: i + 1});
+    }
+  }
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+deck = shuffle(deck);
+for (var p of players) {
+  p.actual_hand = [];
+  for (var i = 0; i < 5; i++) {
+    p.actual_hand.push(deck.pop());
+  }
+}
+
+var game = document.createElement("div");
+
+for (let player of players) {
+  var container = document.createElement("div");
+  container.style.display = "flex";
+  container.style.margin = "10px";
+  container.style.gap = "10px";
+
+  for (var card of player.actual_hand) {
+    let div = document.createElement("div");
+    card.div = div;
+
+    setStyle(div, {
+      width: `${CARD_WIDTH}px`,
+      height: `${CARD_HEIGHT}px`,
+      border: "solid 3px black",
+      "background-color": "lightgray",
+      "border-radius": "5px",
+      "box-shadow": "5px 5px 3px lightgray",
+    });
+    displayCard(card);
+
+    container.appendChild(div);
+  }
+
+  player.handDiv.appendChild(container);
+}
+screen.appendChild(game);
+
+document.body.appendChild(screen);
